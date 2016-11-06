e8e4ce8f1532a7bcc2121685c8ee7575c42772c5
sagacitysite <carlo.michaelis@gmail.com>
2016-11-06 12:22:05 +0100

    immediate result, ex2

--- Block1/2.R
+++ Block1/2.R
@@ -1,17 +1,63 @@
 # 1. ###########################################################################
 
 # Monte-Carlo approximation of pi
-n <- 1000000
-x1 <- runif(n, min = -1, max = 1)
-x2 <- runif(n, min = -1, max = 1)
-indicator <- ((x1^2 + x2^2) <= 1) * 4
-result <- mean(indicator)
+fnMcPi <- function(n, lPlotPoints = 10000) {
+  # Create random uniform values for x and y axis
+  x1 <- runif(n, min = -1, max = 1)
+  x2 <- runif(n, min = -1, max = 1)
+  
+  # Indicate which values are inside of the circle and which are not
+  # Multiply by 4 to have 1 in sum again (area size of 2*2 = 4)
+  indicator <- ((x1^2 + x2^2) <= 1) * 4
+  
+  # Check if points to plot are lower than sample size
+  # If so, reduce sample size
+  if(lPlotPoints > n) {
+    lPlotPoints <- n
+  }
+  
+  # Define reduced values for plotting
+  s1 <- x1[1:lPlotPoints]
+  s2 <- x2[1:lPlotPoints]
+  
+  # Indicate which values are inside of the cirelce and which are outside
+  inside <- ((s1^2 + s2^2) <= 1)
+  outside <- !inside
+  
+  # plot points inside and outside
+  plot(s1[inside], s2[inside], pch = 20, cex = 0.5, col = "red",
+       xlab = "x", ylab = "y",
+       main = paste("Monte-Carlo simulation with sample size of ", n,
+                    " results in pi value of ", mean(indicator), sep = ""))
+  points(s1[outside], s2[outside], pch = 20, cex = 0.5, col = "blue")
+  
+  # plot circle points to mark the border
+  points(sin(1:10000), cos(1:10000), pch = 20, cex = 0.2, col = "black")
+  return(list(s1,s2))
+}
 
-# TODO
-# quadratic format: dev.new(width = 5, height = 5)
-# different colors of points
-# circle line
-plot(x1[1:10000],x2[1:10000], pch = 20, cex = 0.5)
+X <- fnMcPi(1000000)
+
+# Compute mean with each new sample
+vecMeans <- cumsum(X[[1]]) / (1:length(X[[1]]))
+
+# Plot means against number of samples
+plot(1:length(X[[1]]), vecMeans,
+     xlab = "Number of Monte-Carlo samples",
+     ylab = "Monte-Carlo approximation",
+     pch = 4,
+     cex = 0.8)
+abline(vecMeans[length(X[[1]])], 0, col = "red")
+
+vecVariances <- NULL
+for(i in 1:length(X[[1]])) {
+  vecVariances[i] <- var(X[[1]][1:i])
+}
+
+cTop <- vecMeans[length(X[[1]])] - 2*sqrt(length(X[[1]])*vecVariances)
+#cBottom <- 
+lines(cTop, col = "red")
+
 
 # 2. ###########################################################################
 
